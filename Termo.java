5a1dc1cff5222ed18cb158a7dd20d44767085cfc
myrigit <95774660+myrigit@users.noreply.github.com>
2025-02-19 13:00:17 +0100

    coding

--- Termo.java
+++ Termo.java
@@ -9,21 +9,30 @@ public class Termo {
         this.unidad=unidad;
     }
    
-    //Métodos set y get
+  //Métodos set y get
     public void setTemp(double temperatura){
         this.temperatura=temperatura;
     }
+    public void setUnidad(String unidad){
+        this.unidad=unidad;
+    }
+
     public double getTemp(){
         return this.temperatura;
     }
+    public String getUnidad(){
+        return this.unidad;
+    }
     
     //Método convertir
-    public double convertir(double t){
-        System.out.print
+    public double convertir(){
+        if setUnidad=C  
+            double tempF=(setTemperatura*9/5)+32;
+        elif setUnidad=F 
+            double tempC=(setTemperatura-32)+5/9;
+        else
+            System.out.println("Unidad no válida");
     }
 
-
-
-    (1 °C × 9 / 5) + 32 = 33,8 °F
 
 }
