67676953219f13a6ad50c4e18bcafe48a015a81a
myrigit <95774660+myrigit@users.noreply.github.com>
2025-02-21 09:46:05 +0000

    coding

---
+++ Producto.java
@@ -0,0 +1,14 @@
+public class Product{
+    //Atributos
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    //Constructor
+    public Product (String nombre, double precio, int stock){
+        this.nombre=nombre;
+        this.precio=precio;
+        this.stock=stock;
+    }
+    
+}
