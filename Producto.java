cbcb710e8cf79f2c25a5eca761a1578adc87a45a
myrigit <95774660+myrigit@users.noreply.github.com>
2025-02-21 10:00:07 +0000

    end

--- Producto.java
+++ Producto.java
@@ -10,5 +10,44 @@ public class Product{
         this.precio=precio;
         this.stock=stock;
     }
-    
+
+    //Métodos set
+    public void setNombre(String n){
+        this.nombre=n;
+    }
+    public void setPrecio(double p){
+        this.precio=p;
+    }
+    public void setStock(int s){
+        this.stock=s;
+    }
+
+    //Métodos get
+    public String getNombre(){
+        return this.nombre;
+    }
+    public double getPrecio(){
+        return this.precio;
+    }
+    public int getStock{
+        return this.stock;
+    }
+
+    //Método vender
+    public boolean vender(int cantidad){
+        if(cantidad<=this.stock){
+            this.stock -= cantidad;
+            return true;
+        }
+        else{
+            System.out.println("No hay stock suficiente");
+            return false;
+        }
+    }
+
+    //Método reponer
+    public reponer(int cantidad){
+        this.stock += cantidad;
+    }
+
 }
