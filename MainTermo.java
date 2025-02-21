3149c426b2ed9b6d4345e20bbf4657d7e6d2c5f5
myrigit <95774660+myrigit@users.noreply.github.com>
2025-02-21 07:31:55 +0100

    Update MainTermo.java

--- MainTermo.java
+++ MainTermo.java
@@ -4,7 +4,9 @@ public class MainTermo {
         //Creación del objeto (instancia)
         Termo termo1 = new Termo(25,"C"); //para entenderme, termo1 es una variable de tipo Termo
     //Llamada al método convertir
-    convertir(termo1.temperatura,termo1.unidad);
-    System("Temperatura original " + )
+    
+    System.out.println("Temperatura original " +termo1.getTemp() +" " + termo1.getUnidad());
+    System.out.println("Temperatura convertida " +termo1.convertir() +" " +termo1.getUnidad());
+    System.out.println("Temperatura de nuevo convertida " +termo1.convertir() +" " +termo1.getUnidad());
     }
 }
