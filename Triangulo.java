278660a2a4ba1c5f5a94db57945ff235ef05059b
myrigit <95774660+myrigit@users.noreply.github.com>
2025-02-22 06:27:50 +0000

    end

---
+++ Triangulo.java
@@ -0,0 +1,16 @@
+public class Triangulo {
+    //Método forma asteriscos
+    public void forma(){
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+       }
+      //  return  no hace falta porque es void
+    }
+    public static void main(String[] args) {
+        Triangulo tri=new Triangulo();
+        tri.forma();
+    }
+}
