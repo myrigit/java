7efe5b33adf576381524b2046f9d687a986c75f8
myrigit <95774660+myrigit@users.noreply.github.com>
2025-02-24 06:37:04 +0000

    coding

---
+++ ArrayList/Biblioteca/Libro.java
@@ -0,0 +1,36 @@
+package Biblioteca;
+public class Libro {
+    //Atributos
+    private String titulo;
+    private String ISBN;
+    private int pag;
+
+    //Método Constructor
+    public Libro (String titulo, String ISBN, int pag){
+        this.titulo=titulo;
+        this.ISBN=ISBN;
+        this.pag=pag;
+    }
+
+    //Métodos set
+    public void setTitulo(String titulo){
+        this.titulo=titulo;
+    }
+    public void setISBN(String ISBN){
+        this.ISBN=ISBN;
+    } 
+    public void setNumPag(int pag){
+        this.pag=pag;
+    }  
+
+    //Métodos get
+    public String getTitulo(){
+        return this.titulo;
+    }
+    public String getISBN(){
+        return this.ISBN;
+    }
+    public int getNumPag(){
+        return this.pag;
+    }
+}
