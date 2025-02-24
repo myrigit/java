e27afd1a708fd89586cc103c88f9084f8f51cd24
myrigit <95774660+myrigit@users.noreply.github.com>
2025-02-24 07:08:17 +0000

    coding

--- ArrayList/Biblioteca/Biblioteca.java
+++ ArrayList/Biblioteca/Biblioteca.java
@@ -1,4 +1,3 @@
-package Biblioteca;
 import java.util.ArrayList;
 public class Biblioteca {
     //Atributos
@@ -9,12 +8,14 @@ public class Biblioteca {
     public void añadirlibroCreado(Libro book){
         this.libros=new ArrayList<>();
         libros.add(book);
+        numLibros++;
     }
 
     //Método añadir libro   
     public void añadirLibro(String titulo, int numPaginas, String estado){
         Libro nuevoLibro = new Libro(titulo, "sin ISBN", numPaginas);
         libros.add(nuevoLibro);
+        numLibros++;
     }
 
     //Método borrar libro   
@@ -22,6 +23,7 @@ public class Biblioteca {
         for(int i=0;i<libros.size();i++){
             if(libros.get(i).getTitulo().equals(titulo)){
                 libros.remove(i);
+                numLibros--;
                 return 1;
             }
 
